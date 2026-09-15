b40030759124b1f30f3f790248f94c8cf026f3ec
Julieta Patroni <julietapatroni15@gmail.com>
2026-09-14 22:33:18 -0300

    Guardo cambios del formulario antes de combinar auxiliar

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TPWinForm_equipo_6
 {
-    partial class Form1
+    partial class dgvArticulos
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,19 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dataGridView1 = new DataGridView();
+            btnAgregar = new Button();
+            btnModificar = new Button();
+            btnVerDetalle = new Button();
+            btnEliminar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // Form1
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(69, 12);
+            dataGridView1.MultiSelect = false;
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.Size = new Size(672, 206);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.ReadOnlyChanged += dataGridView1_ReadOnlyChanged;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(105, 262);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(75, 23);
+            btnAgregar.TabIndex = 1;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            btnModificar.Location = new Point(257, 262);
+            btnModificar.Name = "btnModificar";
+            btnModificar.Size = new Size(75, 23);
+            btnModificar.TabIndex = 2;
+            btnModificar.Text = "Modificar";
+            btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnVerDetalle
+            // 
+            btnVerDetalle.Location = new Point(417, 262);
+            btnVerDetalle.Name = "btnVerDetalle";
+            btnVerDetalle.Size = new Size(75, 23);
+            btnVerDetalle.TabIndex = 3;
+            btnVerDetalle.Text = "Ver detalle";
+            btnVerDetalle.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(600, 262);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(75, 23);
+            btnEliminar.TabIndex = 4;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += button4_Click;
+            // 
+            // dgvArticulos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Name = "Form1";
-            Text = "Form1";
+            Controls.Add(btnEliminar);
+            Controls.Add(btnVerDetalle);
+            Controls.Add(btnModificar);
+            Controls.Add(btnAgregar);
+            Controls.Add(dataGridView1);
+            Name = "dgvArticulos";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Catálogo de artículos";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Button btnAgregar;
+        private Button btnModificar;
+        private Button btnVerDetalle;
+        private Button btnEliminar;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,28 @@
 namespace TPWinForm_equipo_6
 {
-    public partial class Form1 : Form
+    public partial class dgvArticulos : Form
     {
-        public Form1()
+        public dgvArticulos()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_ReadOnlyChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
         {
 
         }
