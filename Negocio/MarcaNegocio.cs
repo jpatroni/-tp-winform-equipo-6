d7e54220d3808617c86d4e33f5ff8c3c38e1ae21
RomiMG <rominamgiraldo@gmail.com>
2026-09-16 00:57:39 -0300

    Avanzo con las marcas

---
+++ Negocio/MarcaNegocio.cs
@@ -0,0 +1,38 @@
+﻿using Dominio;
+using TPWinForm_equipo_6.Datos;
+
+namespace TPWinForm_equipo_6.Negocio
+{
+    public class MarcaNegocio
+    {
+        public List<Marca> Listar()
+        {
+            var marcas = new List<Marca>();
+
+            using var datos = new AccesoDatos();
+
+            datos.SetearConsulta("""
+                SELECT Id, Descripcion
+                FROM MARCAS
+                ORDER BY Id
+                """);
+
+            datos.EjecutarLectura();
+
+            var lector = datos.Lector;
+
+            while (lector.Read())
+            {
+                var marca = new Marca
+                {
+                    Id = (int)lector["Id"],
+                    Descripcion = lector["Descripcion"] as string ?? string.Empty
+                };
+
+                marcas.Add(marca);
+            }
+
+            return marcas;
+        }
+    }
+}
